d553fa7f01b2704cdd530fb91cca2bab176016bd
Amos Godwin <amosgodwin500@gmail.com>
2024-08-17 00:32:37 +0100

    added the network layer

---
+++ lib/core/network/app_interceptor.dart
@@ -0,0 +1,15 @@
+
+import 'package:dio/dio.dart';
+
+class AppInterceptor extends Interceptor {
+  AppInterceptor();
+
+    @override
+    void onResponse(Response response, ResponseInterceptorHandler handler) {
+      if (response.statusCode! >= 200 &&
+          response.statusCode! < 400) {
+        response.statusCode = 200;
+      }
+      return super.onResponse(response, handler);
+    }
+  }
